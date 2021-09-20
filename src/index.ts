1ab624f6ef8ceade664d195ae5cb539d529c6f37
tenpaMk2 <wavyroom@gmail.com>
2021-09-20 21:57:46 +0900

    :construction: add level.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { Color, Engine, Random, Timer, Vector } from "excalibur";
-import { Block } from "./block";
-import { Ball } from "./ball";
+import { Engine } from "excalibur";
 import { loader } from "./resource";
-import { TwoPI } from "excalibur/dist/Util";
-import { Paddle } from "./paddle";
+import { Level } from "./level";
 
 const game = new Engine({
   width: 800,
@@ -11,63 +8,7 @@ const game = new Engine({
 });
 game.isDebug = true;
 
-let blocks = [];
-const setupBlocks = (game: Engine, width: number, height: number) => {
-  const numOfRow = 5;
-  const numOfColumn = 10;
-  const blockWidth = width / numOfRow;
-  const blockHeight = (height / numOfColumn) * 0.5;
-  const xs = [...Array(numOfRow).keys()].map((num) => num * blockWidth);
-  const ys = [...Array(numOfColumn).keys()].map((num) => num * blockHeight);
-  ys.forEach((y) => {
-    xs.forEach((x) => {
-      const block = new Block(
-        x,
-        y,
-        blockWidth - 5,
-        blockHeight - 5,
-        Color.DarkGray
-      );
-      blocks.push(block);
-      game.add(block);
-    });
-  });
-};
+game.add("level", new Level(game));
+game.goToScene("level");
 
-game.start(loader).then(() => {
-  const block = new Block(0, 0, 100, 100, Color.Red);
-  game.add(block);
-
-  const ball = new Ball(
-    new Vector(game.canvasWidth * 0.2, game.canvasHeight * 0.7),
-    game.canvas.height * 0.02
-  );
-  game.add(ball);
-
-  const timer = new Timer({
-    fcn: () => {
-      const rand = new Random();
-      const r = rand.floating(-TwoPI, TwoPI);
-      const x = Math.cos(r) * 80;
-      const y = Math.sin(r) * 80;
-      ball.vel.x = x;
-      ball.vel.y = y;
-    },
-    interval: 1000,
-    repeats: false,
-  });
-  game.add(timer);
-
-  const paddle = new Paddle(
-    new Vector(game.canvasWidth * 0.5, game.canvasHeight * 0.95),
-    game.canvasWidth * 0.15,
-    game.canvasHeight * 0.02
-  );
-  game.add(paddle);
-
-  game.input.pointers.primary.on("move", (evt) => {
-    paddle.pos.x = evt.worldPos.x;
-  });
-
-  setupBlocks(game, game.canvasWidth, game.canvasHeight);
-});
+game.start(loader).then(() => {});
---
+++ src/level.ts
@@ -0,0 +1,72 @@
+import { Scene, Engine, Vector, Timer, Random, Color } from "excalibur";
+import { TwoPI } from "excalibur/dist/Util";
+import { Ball } from "./ball";
+import { Block } from "./block";
+import { Paddle } from "./paddle";
+
+export class Level extends Scene {
+  blocks: Block[];
+
+  constructor(engine: Engine) {
+    super(engine);
+
+    this.blocks = [];
+  }
+
+  setupBlocks = (engine: Engine, width: number, height: number) => {
+    const numOfRow = 5;
+    const numOfColumn = 10;
+    const blockWidth = width / numOfRow;
+    const blockHeight = (height / numOfColumn) * 0.5;
+    const xs = [...Array(numOfRow).keys()].map((num) => num * blockWidth);
+    const ys = [...Array(numOfColumn).keys()].map((num) => num * blockHeight);
+    ys.forEach((y) => {
+      xs.forEach((x) => {
+        const block = new Block(
+          x,
+          y,
+          blockWidth - 5,
+          blockHeight - 5,
+          Color.DarkGray
+        );
+        this.blocks.push(block);
+        engine.add(block);
+      });
+    });
+  };
+
+  onInitialize(engine: ex.Engine) {
+    const width = engine.drawWidth;
+    const height = engine.drawHeight;
+
+    this.setupBlocks(engine, width, height);
+
+    const ball = new Ball(new Vector(width * 0.2, height * 0.7), height * 0.02);
+    engine.add(ball);
+
+    const timer = new Timer({
+      fcn: () => {
+        const rand = new Random();
+        const r = rand.floating(-TwoPI, TwoPI);
+        const x = Math.cos(r) * 80;
+        const y = Math.sin(r) * 80;
+        ball.vel.x = x;
+        ball.vel.y = y;
+      },
+      interval: 1000,
+      repeats: false,
+    });
+    engine.add(timer);
+
+    const paddle = new Paddle(
+      new Vector(width * 0.5, height * 0.95),
+      width * 0.15,
+      height * 0.02
+    );
+    engine.add(paddle);
+
+    engine.input.pointers.primary.on("move", (evt) => {
+      paddle.pos.x = evt.worldPos.x;
+    });
+  }
+}
