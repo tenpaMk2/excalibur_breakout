b5f71c63d091f1a35a363c38b95057e2d652fd8d
tenpaMk2 <wavyroom@gmail.com>
2021-09-21 20:59:14 +0900

    :construction::recycle: refactoring.

--- src/ball.ts
+++ src/ball.ts
@@ -4,11 +4,18 @@ import {
   Collider,
   CollisionType,
   Color,
+  Engine,
+  PreCollisionEvent,
+  Random,
   Shape,
+  Timer,
   Vector,
 } from "excalibur";
+import { TwoPI } from "excalibur/dist/Util";
 
 export class Ball extends Actor {
+  public killTarget: Actor[];
+
   constructor(pos: Vector, radius: number) {
     super({
       pos: pos,
@@ -20,5 +27,58 @@ export class Ball extends Actor {
         }),
       }),
     });
+
+    this.killTarget = [];
   }
+
+  onInitialize = (engine: Engine) => {
+    this.on("exitviewport", (evt) => {
+      // alert("You lose!");
+    });
+
+    this.on("preCollision", (event: PreCollisionEvent) => {
+      const intersection = event.intersection.normalize();
+
+      if (Math.abs(intersection.x) > Math.abs(intersection.y)) {
+        this.vel.x *= -1;
+      } else {
+        this.vel.y *= -1;
+      }
+
+      if (this.killTarget.indexOf(event.other) > -1) {
+        event.other.kill();
+      }
+    });
+
+    const timer = new Timer({
+      fcn: () => {
+        const rand = new Random();
+        const r = rand.floating(-TwoPI, TwoPI);
+        const x = Math.cos(r) * 200;
+        const y = Math.sin(r) * 200;
+        this.vel.x = x;
+        this.vel.y = y;
+      },
+      interval: 1000,
+      repeats: false,
+    });
+    engine.add(timer);
+  };
+
+  onPostUpdate = (engine: Engine) => {
+    const radius = this.body.collider.localBounds.width / 2;
+    if (this.pos.x < radius) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.x + radius > engine.drawWidth) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y < radius / 2) {
+      this.vel.y *= -1;
+    }
+  };
+
+  addKillTarget = (target: Actor) => {
+    this.killTarget.push(target);
+  };
 }
--- src/level.ts
+++ src/level.ts
@@ -1,5 +1,4 @@
-import { Scene, Engine, Vector, Timer, Random, Color } from "excalibur";
-import { TwoPI } from "excalibur/dist/Util";
+import { Scene, Engine, Vector } from "excalibur";
 import { Ball } from "./ball";
 import { Block } from "./block";
 import { Paddle } from "./paddle";
@@ -29,58 +28,15 @@ export class Level extends Scene {
     });
   };
 
-  onInitialize = (engine: ex.Engine) => {
+  onInitialize = (engine: Engine) => {
     const width = engine.drawWidth;
     const height = engine.drawHeight;
 
     this.setupBlocks(engine, width, height);
 
     const ball = new Ball(new Vector(width * 0.2, height * 0.7), height * 0.02);
+    this.blocks.forEach((block) => ball.addKillTarget(block));
     engine.add(ball);
-
-    ball.on("exitviewport", (evt) => {
-      // alert("You lose!");
-    });
-
-    ball.on("postupdate", (evt) => {
-      const radius = ball.body.collider.localBounds.width / 2;
-      if (ball.pos.x < radius) {
-        ball.vel.x *= -1;
-      }
-      if (ball.pos.x + radius > engine.drawWidth) {
-        ball.vel.x *= -1;
-      }
-      if (ball.pos.y < radius / 2) {
-        ball.vel.y *= -1;
-      }
-    });
-
-    ball.on("precollision", (ev) => {
-      if (this.blocks.indexOf(ev.other) > -1) {
-        ev.other.kill();
-      }
-      const intersection = ev.intersection.normalize();
-
-      if (Math.abs(intersection.x) > Math.abs(intersection.y)) {
-        ball.vel.x *= -1;
-      } else {
-        ball.vel.y *= -1;
-      }
-    });
-
-    const timer = new Timer({
-      fcn: () => {
-        const rand = new Random();
-        const r = rand.floating(-TwoPI, TwoPI);
-        const x = Math.cos(r) * 200;
-        const y = Math.sin(r) * 200;
-        ball.vel.x = x;
-        ball.vel.y = y;
-      },
-      interval: 1000,
-      repeats: false,
-    });
-    engine.add(timer);
 
     const paddle = new Paddle(
       new Vector(width * 0.5, height * 0.95),
